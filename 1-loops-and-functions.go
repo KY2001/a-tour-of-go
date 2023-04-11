e19a903e543582a52b3ddff4bceb76b988b58acb
KY2001 <yukiyoshikein@gmail.com>
2023-04-11 16:33:31 +0900

    update 1-loops-and-functions.go

--- 1-loops-and-functions.go
+++ 1-loops-and-functions.go
@@ -9,11 +9,11 @@ func Sqrt(x float64) float64 {
 	const allowedError = 1e-10
 	z := 1.0
 	for {
-		z_next := z - (z*z-x)/(2*z)
-		if math.Abs(z-z_next) < allowedError {
+		delta := (z*z - x) / (2 * z)
+		if math.Abs(delta) < allowedError {
 			break
 		} else {
-			z = z_next
+			z -= delta
 		}
 	}
 	return z
